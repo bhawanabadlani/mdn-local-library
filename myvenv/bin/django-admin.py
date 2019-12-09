c3f2acce0ae0cba47d01a98949aeaae8807fcdbe
bhawanabadlani <bhawanabadlani@gmail.com>
2019-12-09 12:12:09 +0530

    Creating a skeleton website

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/jpsofficedev/workspace/jpsoffice/bhavesvari/MDN_DjangoTutorial/myvenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
