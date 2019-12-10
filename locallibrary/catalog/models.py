b54e27272f674f936bd395052adae520b44cd696
bhawanabadlani <bhawanabadlani@gmail.com>
2019-12-10 16:08:09 +0530

    Django admin site

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -11,7 +11,7 @@ class Genre(models.Model):
     def __str__(self):
         """String for representing the model object"""
         return(self.name)
-        
+    
 class Language(models.Model):
     """Model representing a Language (e.g. English, French, Japanese, etc.)"""
     name = models.CharField(max_length=200,
@@ -35,6 +35,13 @@ class Book(models.Model):
     # ManyToManyField used because genre can contain many books. Books can cover many genres.
     # Genre class has already been defined so we can specify the object above.
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
+
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+    
+    display_genre.short_description = 'Genre'
     
     def __str__(self):
         """String for representing the Model object."""
@@ -78,6 +85,7 @@ class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField(null=True, blank=True)
 
     class Meta:
         ordering = ['last_name', 'first_name']
